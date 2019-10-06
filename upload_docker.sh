4b0d0422ce7a62486e69877f82dc0094558bb2d6
Karim Garcia <karimg75@gmail.com>
2019-10-06 17:43:43 -0400

    Init

---
+++ upload_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=karimg75/microservices
+export dockerpath="karimg75/microservices"
+
+# Step 2:  
+# Authenticate & tag
+docker login --username karimg75
+docker tag microservices $dockerpath
+echo "Docker ID and Image: $dockerpath" 
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
